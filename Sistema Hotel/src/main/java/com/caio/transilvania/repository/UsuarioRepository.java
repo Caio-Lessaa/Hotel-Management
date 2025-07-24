244bf4be56bf9612a8c6ede629c6911dc79f9897
Caio-Lessaa <caio.lessa01@fatec.sp.gov.br>
2025-07-23 22:42:28 -0300

    Autenticação com JWT e Spring Security

--- Sistema Hotel/src/main/java/com/caio/transilvania/repository/UsuarioRepository.java
+++ Sistema Hotel/src/main/java/com/caio/transilvania/repository/UsuarioRepository.java
@@ -1,2 +1,12 @@
-package com.caio.transilvania.repository;public interface UsuarioRepository {
+package com.caio.transilvania.repository;
+
+import com.caio.transilvania.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Optional<Usuario> findByUsuario(String usuario);
 }
